2206d236e2a4b79f6252574b38f0a28a22d42180
Stephen Jochen <stephenjochen2@gmail.com>
2020-03-02 01:16:40 -0500

    added styling to the card

--- react-test/src/components/itemCard.js
+++ react-test/src/components/itemCard.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {addCart} from '../actions'
+import { addCart } from '../actions'
+import './main.css'
 
 const ItemCard = (props) => {
     console.log(props);
-    
+
     return (
         <div>
-            <img src={props.card.img} alt='Battery'/>
-            <h2>{props.card.name}</h2>
-            <p>{props.card.number}</p>
-            <p>{props.card.pricing}</p>
-            <button onClick={() => props.btnClick()}>Add to Cart</button>
+            <div className="card">
+                <div className="image">
+                    <img src={props.card.img} alt='Battery' />
+                </div>
+                <div className="info">
+                    <h2>{props.card.name}</h2>
+                    <p>{props.card.number}</p>
+                    <p>{props.card.pricing}</p>
+                    <button onClick={() => props.btnClick()}>Add to Cart</button>
+                </div>
+            </div>
         </div>
     )
 }
 
-const mapDispatchToProps = (dispatch, ownProps) =>{
-    return{
-        btnClick: () => dispatch(addCart({partNo: ownProps.card.number, price: ownProps.card.pricing}))
-     }
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        btnClick: () => dispatch(addCart({ partNo: ownProps.card.number, price: ownProps.card.pricing }))
+    }
 }
 
 export default connect(null, mapDispatchToProps)(ItemCard);
